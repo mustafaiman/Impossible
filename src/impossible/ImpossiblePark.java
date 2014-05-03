41395b4708d082e4a8f21fd87e596cd971fb441d
ayhun <ayhuntekat@gmail.com>
2014-05-03 15:23:49 +0300

    denememememememem

--- src/impossible/ImpossiblePark.java
+++ src/impossible/ImpossiblePark.java
@@ -17,6 +17,7 @@ public class ImpossiblePark {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        System.out.print("Denemeeeeee");
     }
     
 }
