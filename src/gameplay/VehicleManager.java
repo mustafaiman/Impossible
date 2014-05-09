17a8540c3e110d54bb167e81b33cd3208776cac6
ayhun <ayhuntekat@gmail.com>
2014-05-09 22:13:36 +0300

    vehicle'a başladık bakalım

---
+++ src/gameplay/VehicleManager.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package gameplay;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author ayhun
+ */
+public class VehicleManager {
+    private int difficulty;
+    ArrayList<Vehicle> vehicles;
+    PlayerCar pCar;
+}
